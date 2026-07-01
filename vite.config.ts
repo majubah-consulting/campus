685d25b9bf608fa9ad1c43c8bfcb507d5c518023
Baudry Bahuna <baudry@majubahconsulting.com>
2026-07-01 17:44:48 +0200

    Make branch deployment production-ready

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/campus/',
   plugins: [react()],
+  build: {
+    rollupOptions: { input: 'index.source.html' },
+  },
   server: { host: '127.0.0.1', port: 4173 },
 })
